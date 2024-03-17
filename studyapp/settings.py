8004c3636b8cae0017602ebfce2336a4cbf81d14
yash kesharwani <imyash2106@gmail.com>
2024-03-17 23:26:58 +0530

    render

--- studyapp/settings.py
+++ studyapp/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
+
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,13 +81,25 @@ WSGI_APPLICATION = 'studyapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER' : 'postgres',
+#         'PASSWORD': 'FlfARxAvPxVQObrWyPvuwiBHBSPhbvKx',
+#         'HOST' : 'viaduct.proxy.rlwy.net',
+#         'PORT' : '47622'
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default" : dj_database_url.parse("postgres://studybuddy_h9w5_user:f5OFS0PMGxl9QnnW9HedqLgXxgdd7Mox@dpg-cnri70sf7o1s73co0v1g-a.oregon-postgres.render.com/studybuddy_h9w5")
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- studyapp/urls.py
+++ studyapp/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
+# from django.contrib.staticfiles import 
 
+from studyapp.settings import MEDIA_URL,STATIC_URL
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
     path('api/',include('base.api.urls'))
 ]
+# urlpatterns += staticfiles_urlpatterns(MEDIA_URL,STATIC_URL)
