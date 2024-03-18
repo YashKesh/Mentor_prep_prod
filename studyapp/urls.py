49533ef95be11e0ed92bcbd06dd004a689f7a552
yash kesharwani <imyash2106@gmail.com>
2024-03-18 11:10:08 +0530

    for deployement again

--- studyapp/urls.py
+++ studyapp/urls.py
@@ -2,6 +2,10 @@
 from django.contrib import admin
 from django.urls import path,include
 # from django.contrib.staticfiles import 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
 
 from studyapp.settings import MEDIA_URL,STATIC_URL
 urlpatterns = [
@@ -10,3 +14,5 @@ urlpatterns = [
     path('api/',include('base.api.urls'))
 ]
 # urlpatterns += staticfiles_urlpatterns(MEDIA_URL,STATIC_URL)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
